6509155ffe54a78135bd86bb0d65d35173a07667
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-09-28 17:16:28 +0530

    init

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var (
+	connectedPublicClients = make(map[string]*websocket.Conn)
+	userName               string
+	err                    error
+)
+
+func main() {
+	router := gin.New()
+	router.POST("user-login", Login)
+	router.GET("ws/chat", Chat)
+	router.Run()
+}
+
+func Login(c *gin.Context) {
+	userName = c.Request.FormValue("userName")
+	if _, ok := connectedPublicClients[userName]; ok {
+		c.JSON(http.StatusBadRequest, "user already exsist")
+	}
+	c.Set("userName", userName)
+}
+
+func Chat(c *gin.Context) {
+	userName = c.GetString("userName")
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("socket connection err :", err)
+		return
+	}
+	defer func() {
+		delete(connectedPublicClients, userName)
+		conn.Close()
+	}()
+	if _, ok := connectedPublicClients[userName]; !ok {
+		connectedMessage := []byte("Connected to the server")
+		err = conn.WriteMessage(websocket.TextMessage, connectedMessage)
+		if err != nil {
+			log.Println("Error sending message:", err)
+			return
+		}
+		connectedPublicClients[userName] = conn
+	}
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			return
+		}
+		fmt.Println(p)
+		for clientName, clientConn := range connectedPublicClients {
+			if clientName != userName {
+				err = clientConn.WriteMessage(messageType, p)
+				if err != nil {
+					log.Println("Error forwarding message:", err)
+					return
+				}
+			}
+		}
+	}
+}
